b98c5d535d3d70fa531baae44399cd3910f3cbde
Itai Weisman <itai.weisman@gmail.com>
2018-03-18 13:31:43 +0200

    Update regall_dr.ps1

--- regall_dr.ps1
+++ regall_dr.ps1
@@ -1,10 +1,10 @@
-$vcenter="vcsa-sales-001"
-$datastore="replicated-sol-prod-datastore"
-$cluster="Sales"
-$esx="sales-demo-001.lab.il.infinidat.com"
+$vcenter="<replace_with_vcenter_name>"
+$datastore="<replace_with_datastore_name" ## or $args[0] 
+$cluster="<replace_with_cluster_name"
+$esx="<replace_with_esx_name>"
 $cred=Get-Credential
-$suffix="-new"
-Connect-VIServer -Credential $cred -server vcsa-sales-001
-dir "vmstores:\$vcenter@443\$cluster\$datastore\*\io*.vmx" | % {
+$suffix="-new"  ## or what makes sense
+Connect-VIServer -Credential $cred -server $vcenter
+dir "vmstores:\$vcenter@443\$cluster\$datastore\*\*.vmx" | % {
 $name=$_.Name.Split(".")[0]+ "$suffix" 
 New-VM -Host $esx -Name $name -VMFilePath $_.DatastoreFullPath }
