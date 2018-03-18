fa8e0540c5187c1305ae887781c6832508b8dd93
Itai Weisman <itai.weisman@gmail.com>
2018-03-18 13:33:22 +0200

    Update chrole_all.ps1

--- chrole_all.ps1
+++ chrole_all.ps1
@@ -1,8 +1,8 @@
-$ibox="ibox628"
-$user="admin"
-$password="123456"
-$file=$args[0]
-$dss=get-content $file
+$ibox="<replace_with_ibox_name>"
+$user="<replace_with_ibox_username>"
+$password="<replace_with_ibox_password>"
+$file="<replace_with_a_file_containing_ds_name>" ## $args[0]
+$dss=get-content $file 
 foreach ($ds in $dss) { 
 write-host "Trying to change role of $ds "
 infinishell --user $user --password $password -c "replica.change_role local_dataset=$ds --yes" $ibox
