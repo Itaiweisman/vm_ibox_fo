56de86d24435f1ccf23b85a532478d9222e56b73
Itai Weisman <itai.weisman@gmail.com>
2018-03-21 11:07:27 +0200

    Update resume_rep_all.ps1

--- resume_rep_all.ps1
+++ resume_rep_all.ps1
@@ -6,12 +6,10 @@ $user_ibox2="<replace_with_ibox_username>"
 $password_ibox2="<replace_with_ibox_password>"
 $file="<replace_with_a_file_containing_ds_name>" ## $args[0]
 $dss=get-content $file 
-foreach ($ds in $dss) { 
-write-host "Trying to change role of $ds[1] "
-infinishell --user $user2 --password $password2 -c "replica.change_role local_dataset=$ds[1] --yes" $ibox2_to_replicte_to
- }
- 
- foreach ($ds in $dss) { 
-write-host "Trying to replicate from $ds[0]  "
-infinishell --user $user1 --password $password1 -c "replica.resume local_dataset=$ds[0] --yes" $ibox1_to_replicte_to
+foreach ($ds in $dss) {
+$source, $target=$ds.Split(' ')
+write-host "Trying to change role of ds is $target "
+infinishell --user $user_ibox2 --password $password_ibox2 -c "replica.change_role local_dataset=$target --yes" $ibox2_to_replicte_to
+write-host "Trying to replicate from $source  "
+infinishell-ve/bin/infinishell --user $user_ibox1 --password $password_ibox1 -c "replica.resume local_dataset=$source --yes" $ibox1_to_replicte_from
  }
