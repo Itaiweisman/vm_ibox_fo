0f0252ee4d7f4f238773d18d2a3137016686687e
Itai Weisman <itai.weisman@gmail.com>
2018-04-01 13:46:06 +0300

    Update start_vms.ps1

--- start_vms.ps1
+++ start_vms.ps1
@@ -1,6 +1,16 @@
 $vm_list=get-content $args[0] ## File with vm list, sepearted by new line
 foreach ($vm in $vm_list) {
-  Start-VM $vm
-  Get-VM $vm | Get-VMQuestion | Set-VMQuestion -Option "I moved it" 
-  }
+  try {
+    start-vm $vm -ErrorAction Stop
+}
+catch {
+    $message = $_.Exception
+
+
+    if ($message -like "*questions*") {
+        $question = $vm | Get-VMQuestion
+        $option = $question.Options | Where-Object {$_.Summary -eq "I Moved It"}
+        $vm | Get-VMQuestion | Set-VMQuestion -Option $option
+    }
+}
   
